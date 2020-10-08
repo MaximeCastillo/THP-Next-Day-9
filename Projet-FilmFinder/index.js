b600b3eff3ad47d580c2e087a1287c99d31883b7
TonyStark992 <maxime.castillo@gmail.com>
2020-10-08 21:04:58 +0200

    searchMovie() async/await or .then

--- Projet-FilmFinder/index.js
+++ Projet-FilmFinder/index.js
@@ -1,1 +1,28 @@
-const response = await fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchedMovie}`);
+// APIKEY définie dans apiKey.js
+
+const searchedMovie = "Iron Man"
+
+const URL = `http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchedMovie}`
+
+const myFunction = () =>
+{
+    console.log("Texte de myFunction");
+}
+
+const searchMovie = async () => {
+    const response = await fetch(URL);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+}
+
+const searchMovie2 = () => {
+    fetch(URL)
+        .then ((response) => {
+            console.log(response);
+            const data = response.json();
+            console.log(data);
+            return data;
+        })
+        .then ((response) => console.log(response))
+}
